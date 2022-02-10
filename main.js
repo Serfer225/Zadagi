7c8621fe0085d6fd7ba7b658e73c99259389f1c9
Serfer225 <serf0428@gmail.com>
2022-02-10 21:06:04 +0200

    Zadaji

---
+++ main.js
@@ -0,0 +1,57 @@
+//Task1
+// let k = Number(prompt("Введите число K"))
+// let n = Number(prompt("Введите желаное сколько раз вывести число K"))
+// for(n>0;n--;n<k){
+//     console.log(k)
+// }
+
+//Task2
+// let a = prompt("Введите число а")
+// let b = Number(prompt("Введите число b"))
+// for(n=a;n<=b;n++){                                 
+//      console.log(n)
+// }
+
+
+//Task3
+// let a = prompt("Введите число а")
+// let b = Number(prompt("Введите число b"))
+// for(n=b-1;n>a;n--){                                 
+//     console.log(n)
+// }
+
+//Task4
+// let c = prompt("Введите цену за 1кг конфет")
+// for (let a=1; a<=10 ; a+=1 ){
+//     console.log(`Стоимость за ${a} кг, я должен заплатить ${c*a}`)
+// }
+
+//Task5
+//  let c = prompt("Введите цену за 1кг кофет")
+//  for (let a=0.1; a<=1 ; a+=0.1 ){
+//      console.log(`Стоимость за ${a} кг, я должен заплатить ${(c/10)*a}`)
+// }
+
+//Task6
+// let c = prompt("Введите цену за 1кг кофет")
+// for (let a=1.2; a<=2 ; a+=0.2 ){
+//     console.log(`Стоимость за ${a} кг, я должен заплатить ${(c/5)*a}`)
+// }
+
+//Task7
+// let a = prompt("Введите число а")
+// let b = prompt("Введите число b")
+// let g = a+1
+// let d = 0
+// for(n=a;n<=b;g++){                       
+//     d=n+g                               Непонимаю получается бред
+//     console.log(d)
+//     n++
+// }
+
+//Task8    
+                                                //Аналогично с Task7
+//Task9
+
+//Task10
+//Извините но я ничего дальше вообщ не понял
